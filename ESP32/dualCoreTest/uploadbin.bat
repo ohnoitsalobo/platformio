2f14e5c4911678f5d6dc85bb7a957b7668cf8ded
ohnoitsalobo <2175437+ohnoitsalobo@users.noreply.github.com>
2020-08-22 21:40:56 +0530

    update

---
+++ ESP32/dualCoreTest/uploadbin.bat
@@ -0,0 +1,1 @@
+pio run && "c:\python38\python.exe" "C:\Users\Anand\.platformio\packages\framework-arduinoespressif32\tools\espota.py" --debug --progress -i 192.168.43.153 -f .pio\build\bpi-bit\firmware.bin
